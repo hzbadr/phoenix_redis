93c7d38ae5ec955bb7314f1f94f12ae7f99699af
Hassan Zaki <hassan.zaki@badrit.com>
2016-06-17 01:34:55 +0200

    exrm

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ config :saas_realtime_api, SaasRealtimeApi.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
-  server: true
+  server: true,
+  code_reloader: false,
 
 # Do not print debug messages in production
 config :logger, level: :info

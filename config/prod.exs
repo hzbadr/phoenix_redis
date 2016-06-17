dbcc35d6bbb6cbf00a78cfbfa75d505637b79d5b
Hassan Zaki <hassan.zaki@badrit.com>
2016-06-17 21:38:07 +0200

    fix

--- config/prod.exs
+++ config/prod.exs
@@ -66,10 +66,19 @@ config :logger, level: :info
 # which should be versioned separately.
 import_config "prod.secret.exs"
 
-config :exredis,
-  host: "localhost",
+# config :exredis,
+#   host: "localhost",
+#   port: 6379,
+#   password: "",
+#   db: 0,
+#   reconnect: :no_reconnect,
+#   max_queue: :infinity
+config :redis_poolex,
+  host: "127.0.0.1",
   port: 6379,
   password: "",
   db: 0,
   reconnect: :no_reconnect,
-  max_queue: :infinity
+  max_queue: :infinity,
+  pool_size: 10,
+  pool_max_overflow: 1
